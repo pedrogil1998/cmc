0c298cd2e5cda727de1867db7223ce7470f5331b
pedromoura <pedrogilmoura27@gmail.com>
2023-12-29 11:52:58 +0000

    CMC-07 improvements

--- cmcup/src/App.jsx
+++ cmcup/src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useDownloadExcel } from "react-export-table-to-excel";
-import { addResultsToChampionship } from "./utils/utils";
+import { addResultsToChampionship, scoringSystem } from "./utils/utils";
 import {
   Box,
+  Button,
+  Divider,
   Paper,
   Table,
   TableBody,
@@ -125,23 +127,32 @@ function App() {
         flexDirection: "column",
       }}
     >
-      <Box display="flex" flexDirection="row" justifyContent="space-between">
-        <TitleText variant="h1" style={{ color: "black" }}>
-          CABO DO MUNDO CUP
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="2rem"
+      >
+        <TitleText variant="h2" style={{ color: "black" }}>
+          Classificações
         </TitleText>
         <a href="https://cabodomundokarting.pt/" target="_blank">
           <img src={kcdmlogo} className="logo cm" alt="Cm logo" />
         </a>
       </Box>
-      <Box display="flex" flexDirection="column" alignItems="center">
+      <Box display="flex" flexDirection="column" alignItems="center" sx={{backgroundColor: "lightgray"}}>
+        <TitleText variant="h5" style={{ color: "black", marginTop:"3rem" }}>
+          Inserir os ficheiros em formato <u>.csv</u>:
+        </TitleText>
         <Box
           margin="1rem"
           padding="1rem"
           borderRadius="10px"
-          sx={{ backgroundColor: "lightslategray", width: "fit-content" }}
+          sx={{ backgroundColor: "#a0a0a0", width: "fit-content" }}
         >
           <input ref={inputRef} type="file" onChange={handleFileChange} />
-          <button onClick={resetFileInput}>Clear</button>
+          <button onClick={resetFileInput} style={{backgroundColor: "ActiveBorder"}}>Clear</button>
         </Box>
         <Box
           display="flex"
@@ -152,13 +163,13 @@ function App() {
           <TableContainer component={Paper}>
             <Table
               ref={tableRef}
-              sx={{ minWidth: 650, backgroundColor: "#339966" }}
+              sx={{ minWidth: 500, backgroundColor: "#649d81" }}
               aria-label="simple table"
             >
               <TableHead>
                 <TableRow>
                   <TableCell>Posição</TableCell>
-                  <TableCell align="left">Nome</TableCell>
+                  <TableCell align="center">Nome</TableCell>
                   <TableCell align="right">Pontuação</TableCell>
                 </TableRow>
               </TableHead>
@@ -175,7 +186,7 @@ function App() {
                     >
                       {piloto.pos}
                     </TableCell>
-                    <TableCell align="left" sx={{ color: "white" }}>
+                    <TableCell align="center" sx={{ color: "white" }}>
                       {piloto.name}
                     </TableCell>
                     <TableCell align="right" sx={{ color: "white" }}>
@@ -189,23 +200,69 @@ function App() {
         </Box>
         <Box display="flex" flexDirection="row">
           <button
-            style={{ marginRight: "1rem", width: "8rem" }}
+            style={{
+              marginRight: "0.5rem",
+              backgroundColor: "Highlight",
+            }}
             onClick={handleSaveChampionship}
           >
             Save
           </button>
           <button
-            style={{ marginLeft: "1rem", marginRight: "1rem", width: "8rem" }}
+            style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }}
             onClick={onDownload}
           >
             Download
           </button>
           <button
-            style={{ marginLeft: "1rem", width: "8rem" }}
+            style={{ marginLeft: "0.5rem" }}
             onClick={handleClearChampionship}
           >
             Limpar Tabela
           </button>
+        </Box>
+        <TitleText variant="h6" style={{ color: "black", marginTop: "0.5rem", marginBottom: "3rem"}}>
+          * Ao clicar no botão <u>Save</u> estará a guardar as alterações na
+          Base de Dados.
+        </TitleText>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center" paddingBottom={"2rem"}>
+        <TitleText marginTop="3rem" variant="h5" style={{ color: "black" }}>
+          Sistema de pontuação utilizado:
+        </TitleText>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          margin="1rem"
+        >
+          <TableContainer component={Paper}>
+            <Table
+              //ref={tableRef}
+              sx={{ minWidth: 200, backgroundColor: "aliceblue" }}
+              aria-label="simple table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Posição</TableCell>
+                  <TableCell align="center">Pontuação</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {scoringSystem.map((piloto, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row" align="center">
+                      {piloto.pos}
+                    </TableCell>
+                    <TableCell align="center">{piloto.points}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </Box>
     </div>
